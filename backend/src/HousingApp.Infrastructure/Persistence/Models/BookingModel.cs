335c98dda1a7f0713fefc04c188a35f05dcf0bad
Rafael Vargas <rafael1199v@gmail.com>
2026-03-13 12:03:35 -0400

    feat: Adding camel case naming convention

--- backend/src/HousingApp.Infrastructure/Persistence/Models/BookingModel.cs
+++ backend/src/HousingApp.Infrastructure/Persistence/Models/BookingModel.cs
@@ -1,5 +1,8 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace HousingApp.Infrastructure.Persistence.Models
 {
+    [Table("bookings")]
     public class BookingModel
     {
         public int Id { get; set; }
