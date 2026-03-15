b03fbefc0060a41800ab6a83aebf70d325741087
Rafael Vargas <rafael1199v@gmail.com>
2026-03-15 17:42:36 -0400

    feat: Get householder rooms use case created

--- backend/src/HousingApp.Application/Room/DTO/RoomHouseholderDto.cs
+++ backend/src/HousingApp.Application/Room/DTO/RoomHouseholderDto.cs
@@ -1,4 +1,14 @@
 namespace HousingApp.Application.Room.DTO
 {
-    public record RoomHouseholderDto();
+    public record RoomHouseholderDto(
+        int Id,
+        string Name,
+        double Latitude,
+        double Longitude,
+        string Description,
+        double Price,
+        string RoomStatus,
+        int BookingRequests,
+        List<string> ImageRoomUrls
+    );
 }
--- backend/src/HousingApp.Application/Room/UseCases/GetHouseholderRoomsUseCase.cs
+++ backend/src/HousingApp.Application/Room/UseCases/GetHouseholderRoomsUseCase.cs
@@ -1,7 +1,28 @@
+using HousingApp.Application.Room.DTO;
+using HousingApp.Domain.Entities;
+using HousingApp.Domain.Repositories;
+
 namespace HousingApp.Application.Room.UseCases
 {
-    public class GetHouseholderRoomsUseCase
+    public class GetHouseholderRoomsUseCase(IRoomRepository roomRepository) : IGetHouseholderRoomsUseCase
     {
-        
+        public async Task<Result<List<RoomHouseholderDto>>> ExecuteAsync(string userId)
+        {
+            List<RoomHouseholder> roomHouseholderEntities = await roomRepository.GetHouseholderRoomsAsync(userId);
+
+            List<RoomHouseholderDto> roomHouseholderDtoList = [.. roomHouseholderEntities.Select(r => new RoomHouseholderDto(
+                Id: r.Id,
+                Name: r.Name,
+                Description: r.Description,
+                Latitude: r.Latitude,
+                Longitude: r.Longitude,
+                Price: r.Price,
+                RoomStatus: r.Status.ToString(),
+                BookingRequests: r.BookingRequests,
+                ImageRoomUrls: r.ImageRoomUrls
+            ))];
+
+            return Result<List<RoomHouseholderDto>>.Success(roomHouseholderDtoList);
+        }
     }
 }
