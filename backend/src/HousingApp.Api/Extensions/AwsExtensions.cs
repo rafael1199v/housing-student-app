f730f32e37c9bff80213369efeb8bf41a4620916
Rafael Vargas <rafael1199v@gmail.com>
2026-03-25 23:56:03 -0400

    feat: Service extensions and global rate limiter added by ip

---
+++ backend/src/HousingApp.Api/Extensions/AwsExtensions.cs
@@ -0,0 +1,31 @@
+using Amazon.Extensions.NETCore.Setup;
+using Amazon.Runtime;
+using Amazon.S3;
+using HousingApp.Application.Storage;
+using HousingApp.Infrastructure.Storage;
+
+namespace HousingApp.Api.Extensions
+{
+    public static class AwsExtensions
+    {
+        public static IServiceCollection AddAwsStorage(this IServiceCollection services, IConfiguration configuration)
+        {
+            AWSCredentials credentials = new BasicAWSCredentials(
+                configuration["AWS:AccessKey"],
+                configuration["AWS:SecretKey"]);
+
+            AWSOptions? awsOptions = configuration.GetAWSOptions();
+            awsOptions.Credentials = credentials;
+
+            services.AddDefaultAWSOptions(awsOptions);
+            services.AddAWSService<IAmazonS3>();
+
+            services.Configure<StorageSettings>(
+                configuration.GetSection("Storage"));
+
+            services.AddScoped<IStorageService, S3StorageService>();
+
+            return services;
+        }
+    }
+}
