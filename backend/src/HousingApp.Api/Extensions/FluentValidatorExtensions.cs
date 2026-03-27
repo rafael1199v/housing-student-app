164490102473a3bc64d1109e4945bd8c3c4524d5
Rafael Vargas <123646379+rafael1199v@users.noreply.github.com>
2026-03-27 00:20:08 -0400

    feat: Fluent validations installed

---
+++ backend/src/HousingApp.Api/Extensions/FluentValidatorExtensions.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace HousingApp.Api.Extensions
+{
+    public static class FluentValidatorExtensions
+    {
+        public static IServiceCollection AddFluentValidation(this IServiceCollection services)
+        {
+
+            services.AddValidatorsFromAssemblyContaining<Program>();
+            return services;
+        }
+    }
+}
