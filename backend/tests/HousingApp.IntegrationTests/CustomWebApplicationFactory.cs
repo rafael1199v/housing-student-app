f4e067434d2c9d04371c1ecdeb6040504b567331
Rafael Vargas <123646379+rafael1199v@users.noreply.github.com>
2026-04-08 11:44:37 -0400

    feat: Web appliction factory for in memory api tests added

--- backend/tests/HousingApp.IntegrationTests/CustomWebApplicationFactory.cs
+++ backend/tests/HousingApp.IntegrationTests/CustomWebApplicationFactory.cs
@@ -1,6 +1,22 @@
-﻿namespace HousingApp.IntegrationTests;
+﻿using HousingApp.Infrastructure.Persistence.Context;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
-public class CustomWebApplicationFactory
+namespace HousingApp.IntegrationTests;
+
+public class CustomWebApplicationFactory(string postgresConnectionString) : WebApplicationFactory<Program>
 {
-    
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+            services.AddDbContext<HousingApplicationDbContext>(options =>
+            {
+                options.UseNpgsql(postgresConnectionString);
+            });
+        });
+    }
 }
