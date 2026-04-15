5f665979302a71b5c4e9f5c024706e5561d05f8e
Rafael Vargas <rafael1199v@gmail.com>
2026-04-15 10:35:50 -0400

    fix: Repository functionality added with repository interaction

--- backend/src/HousingApp.Application/Auth/UseCases/LoginWithRefreshTokenUseCase.cs
+++ backend/src/HousingApp.Application/Auth/UseCases/LoginWithRefreshTokenUseCase.cs
@@ -19,6 +19,8 @@ public class LoginWithRefreshTokenUseCase(IRefreshTokenRepository refreshTokenRe
         }
 
         RefreshToken renewedRefreshToken = refreshToken.Renew();
+        
+        await refreshTokenRepository.UpdateRefreshToken(renewedRefreshToken);
 
         UserDto userDto = new(
             Id: renewedRefreshToken.UserId,
