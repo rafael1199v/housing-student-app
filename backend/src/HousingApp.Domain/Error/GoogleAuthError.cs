134558063109376872fb5bd907a2d4a0db5054b5
Rafael Vargas <123646379+rafael1199v@users.noreply.github.com>
2026-04-21 15:40:43 -0400

    feat: Google errors added

---
+++ backend/src/HousingApp.Domain/Error/GoogleAuthError.cs
@@ -0,0 +1,6 @@
+﻿namespace HousingApp.Domain.Error;
+
+public static class GoogleAuthError
+{
+    public static readonly Error InvalidGoogleToken = new("google.token.invalid", "El token de Google es inválido o ha expirado");
+}
