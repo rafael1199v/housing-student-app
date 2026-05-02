bb7a21ebdffa699eea238b8bb779e5d861b9ef12
Rafael Vargas <123646379+rafael1199v@users.noreply.github.com>
2026-05-02 10:34:48 -0400

    feat: Ef core models updated to create a relation between rooms, policies and services

--- backend/src/HousingApp.Infrastructure/Persistence/Models/PolicyModel.cs
+++ backend/src/HousingApp.Infrastructure/Persistence/Models/PolicyModel.cs
@@ -10,4 +10,5 @@ public class PolicyModel : AuditableModel
     public int Id { get; set; }
     [Required][MaxLength(50)] public string Code { get; set; } = string.Empty;
     [Required][MaxLength(100)] public string Name { get; set; } = string.Empty;
+    public ICollection<RoomModel> Rooms { get; set; } = [];
 }
