ec5973a3175fc275f00d279fe3f75ccd2fb07015
Daniel Roland Peñaranda Colque <eldanielitu@gmail.com>
2026-05-03 19:32:49 -0400

    fix: SVG dictionaries import
    Además de haber regresado atrás con lo de la implementación del backend (pues fue innecesario al final, ya que se requieren datos tanto de backend como de frontend, no tenía sentido traer todo de backend), se corrigió la importación de SVGs para políticas y servicios.

--- frontend/src/shared/icons/services-icons-dictionary.ts
+++ frontend/src/shared/icons/services-icons-dictionary.ts
@@ -1,20 +1,20 @@
+import climateIcon from "../../assets/icons/services/climate_mini_split.svg";
+import exerciseIcon from "../../assets/icons/services/exercise.svg";
+import kitchenIcon from "../../assets/icons/services/kitchen.svg";
+import routerIcon from "../../assets/icons/services/router.svg";
+import tvIcon from "../../assets/icons/services/tv.svg";
+
 interface Service {
 	code: string;
 	icon: string;
 }
 
 const SERVICE_ICONS: Service[] = [
-	{ code: "service.tv", icon: "../../assets/icons/services/tv.svg" },
-	{ code: "service.wifi", icon: "../../assets/icons/services/router.svg" },
-	{ code: "service.kitchen", icon: "../../assets/icons/services/kitchen.svg" },
-	{
-		code: "service.gym-equipment",
-		icon: "../../assets/icons/services/exercise.svg",
-	},
-	{
-		code: "service.air-conditioner",
-		icon: "../../assets/icons/services/climate_mini_split.svg",
-	},
+	{ code: "service.tv", icon: tvIcon },
+	{ code: "service.wifi", icon: routerIcon },
+	{ code: "service.kitchen", icon: kitchenIcon },
+	{ code: "service.gym-equipment", icon: exerciseIcon },
+	{ code: "service.air-conditioner", icon: climateIcon },
 ];
 
 export { SERVICE_ICONS };
