f1514011af0a9105950a7a3f3747a3a2e5fb27c0
Rafael Vargas <123646379+rafael1199v@users.noreply.github.com>
2026-06-14 12:50:23 -0400

    feat: new migrations added with avatar source

---
+++ backend/src/HousingApp.Infrastructure/Migrations/20260614142956_AvatarSource.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HousingApp.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AvatarSource : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "image_source",
+                table: "persons",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "image_source",
+                table: "persons");
+        }
+    }
+}
